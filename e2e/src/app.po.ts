c1cf2ae568e43d78ecd49ee7c9391853d792c0c9
Nick Dolan <nicholas.dolan1@gmail.com>
2018-08-22 12:13:53 -0500

    initial commit

---
+++ e2e/src/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('nick-root h1')).getText();
+  }
+}
