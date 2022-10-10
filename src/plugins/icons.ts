0ca807a61b0f80bbfc17e75021216e0fb652ba47
Nick Dolan <nicholas.dolan1@gmail.com>
2022-10-10 02:21:04 -0500

    Remove existing impl

--- src/plugins/icons.ts
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faHeart } from '@fortawesome/free-solid-svg-icons';
-
-library.add(faHeart);
-
-Vue.component('font-awesome-icon', FontAwesomeIcon);
